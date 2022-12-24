646c2ba9c059491cdc433fea3dfc89765dc56872
tochidoh <tochidoh@gmail.com>
2022-12-24 17:06:49 -0500

    cleanup

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,22 +1,24 @@
+use std::net::TcpListener;
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("failed to bind address");
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
+}
+
 #[tokio::test]
 async fn health_check_works() {
-    // arrange
-    spawn_app();
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
-    // act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("failed to execute request");
 
-    // assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
-}
-
-fn spawn_app() {
-    let server = zero2prod::run().expect("failed to bind address");
-    let _ = tokio::spawn(server);
 }
